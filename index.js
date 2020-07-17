1f1d313baa3c482c90cd429d65dd259212dcf8b0
Club-Nation <piepenschneider.leon@gmail.com>
2020-07-17 19:14:54 +0200

    Second

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 
-client.config = {
-  token: process.env.DISCORD_TOKEN,
-  prefix: process.env.DISCORD_PREFIX,
-  //api: process.env.GOOGLE_API,
-};
-
 client.commands = new Discord.Collection();
 client.queue = new Map();
 
